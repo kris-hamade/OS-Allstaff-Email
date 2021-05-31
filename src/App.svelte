45b78d29c38ed06370724e4d01967448ebe7ddf6
Kris Hamade <slash2223456@gmail.com>
2021-05-31 04:30:54 -0400

    Svelte Init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Preview from "./Preview.svelte";
+</script>
+
+<main>
+	<Preview/>
+</main>
+
+<style>
+
+</style>
---
+++ src/Preview.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { link } from "fs";
+</script>
+
+<main>
+  <html>
+    <head>
+      <script src="jquery.js"></script>
+      <script>
+        $(function () {
+          $("#includedContent").load("b.html");
+        });
+      </script>
+    </head>
+
+    <body>
+      <div id="includedContent" />
+    </body>
+  </html>
+</main>
